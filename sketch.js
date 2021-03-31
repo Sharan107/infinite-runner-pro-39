a9484504f113f5a8b133dea4754d99dff25c791e
Sharan107 <75675225+Sharan107@users.noreply.github.com>
2021-03-31 11:31:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+var track,carImg,obstacleImg;
+var bgImg,car,obstacle;
+var gameState="play";
+
+function preload() {
+
+  //preloading images
+
+ track=loadImage("images/track.jpg");
+ obstacleImg=loadImage("images/stone.png");
+ carImg=loadImage("images/racing car.png");
+  
+}
+
+function setup() {
+  createCanvas(displayWidth-20,displayHeight-30);
+
+  //creating sprites
+
+ obstacle=createSprite(random(150,600),random(300,500),30,30);
+ obstacle.addImage(obstacleImg);
+
+ car=createSprite(displayWidth-20/2,displayHeight-30/2,50,50);
+ car.addImage(carImg);
+ car.setCollider("rectangle",0,100,250,250);
+
+ // console.log(displayHeight)
+ }
+
+function draw() {
+ background(rgb(198,135,103));
+ image(track, 620,-displayHeight*4,displayWidth, displayHeight*5);
+
+ //making car move with arrow keys
+if(gameState==="play"){
+ if(keyDown(LEFT_ARROW)){
+    car.x=car.x-10;
+ }
+if(keyDown(RIGHT_ARROW)){
+  car.x=car.x+10;
+}
+if(keyDown(UP_ARROW)){
+  car.y=car.y-10;
+}
+}
+
+//if the car hits the obstacle game is over  
+if(car.isTouching(obstacle)){ 
+  car.destroy();
+  obstacle.destroy(); 
+  gameState="end"; } 
+
+  //setting camera directions
+camera.position.x = displayWidth-20/2;
+camera.position.y = car.y;
+
+if(gameState==="end"){
+  textSize(40);
+  fill("white")
+  stroke("black");
+  strokeWeight(8);
+  text("GAMEOVER!!!!",650,600);
+}
+
+console.log(displayHeight*5);
+
+if(car.y>=3600){
+  textSize(40);
+  fill("white");
+  stroke("balck");
+  strokeWeight(8);
+  text("YOU WON!!!",650,4000);
+}
+
+  drawSprites();
+}
